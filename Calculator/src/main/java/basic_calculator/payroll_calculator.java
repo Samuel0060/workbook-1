d512445f6463ae9dc27a3fd510d1c0a87d53a02c
Samuel Stephenson <sstephenson10469@gmail.com>
2025-04-14 13:29:06 -0400

    Completed Mod3 - Exercise 4 - Payroll Calculator

--- Calculator/src/main/java/basic_calculator/payroll_calculator.java
+++ Calculator/src/main/java/basic_calculator/payroll_calculator.java
@@ -1,4 +1,21 @@
 package basic_calculator;
 
+import java.util.Scanner;
+
 public class payroll_calculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("What is your name?\n");
+        String name = scanner.next();
+
+        System.out.print("Hours worked?\n");
+        int hours = scanner.nextInt();
+
+        System.out.print("Pay rate?\n");
+        float rate = scanner.nextFloat();
+
+        float grosspay = hours * rate;
+
+        System.out.printf("%s\nGross Pay: $%.2f",name,grosspay);
+    }
 }
